7da24c3d3c85195202b89382978329ed39f2d3e1
rudierimachado <rudierimachado@outlook.com>
2026-03-09 09:38:44 -0300

    DASDA

--- app.py
+++ app.py
@@ -823,4 +823,4 @@ def api_restart_app():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")), debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5009")), debug=True)
